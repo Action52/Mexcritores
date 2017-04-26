58858939de71e9aca940e908caaa2bbece395806
arman <united.abominations95@gmail.com>
2017-04-26 17:34:26 -0500

    primer intento de crud de writer

---
+++ app/listwriters.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Listado de escritores</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    </head>
+    <body>
+        <?php
+        require_once "../models/Writer.php";
+        $db = new Database;
+        $user = new Writer($db);
+        $users = $user->get();
+        ?>
+        <div class="container">
+            <div class="col-lg-12">
+                <h2 class="text-center text-primary">Lista de escritores</h2>
+                <div class="col-lg-1 pull-right" style="margin-bottom: 10px">
+                    <a class="btn btn-info" href="<?php echo Writer::baseurl() ?>/app/add.php">Add user</a>
+                </div>
+                <?php
+                if( ! empty( $users ) )
+                {
+                ?>
+                <table class="table table-striped">
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>E-mail</th>
+                        <th>Edad</th>
+                        <th>Nacionalidad</th>
+                    </tr>
+                    <?php foreach( $users as $user )
+                    {
+                    ?>
+                        <tr>
+                            <td><?php echo $user->id ?></td>
+                            <td><?php echo $user->nombre ?></td>
+                            <td><?php echo $user->apellidos ?></td>
+                            <td><?php echo $user->email ?></td>
+                            <td><?php echo $user->edad ?></td>
+                            <td><?php echo $user->nacionalidad ?></td>
+                            <td>
+                                <a class="btn btn-info" href="<?php echo Writer::baseurl() ?>app/edit.php?user=<?php echo $user->id ?>">Edit</a> 
+                                <a class="btn btn-info" href="<?php echo Writer::baseurl() ?>app/delete.php?user=<?php echo $user->id ?>">Delete</a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </table>
+                <?php
+                }
+                else
+                {
+                ?>
+                <div class="alert alert-danger" style="margin-top: 100px">Any user has been registered</div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ models/Writer.php
@@ -0,0 +1,121 @@
+<?php
+require_once("../database/Database.php");
+require_once("../interfaces/IUser.php");
+
+class Writer implements IUser {
+	private $con;
+    private $id;
+    private $nombre;
+    private $apellidos;
+    private $email;
+    private $edad;
+    private $nacionalidad;
+
+	public function __construct(Database $db){
+		$this->con = new $db;
+	}
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function setnombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function setapellidos($apellidos){
+        $this->apellidos = $apellidos;
+    }
+
+    public function setemail($email){
+        $this->email = $email;
+    }
+
+    public function setedad($edad){
+        $this->edad = $edad;
+    }
+
+    public function setnacionalidad($nacionalidad){
+        $this->nacionalidad = $nacionalidad;
+    }
+
+	//insertamos usuarios en una tabla con postgreSql
+	public function save() {
+		try{
+			$query = $this->con->prepare('INSERT INTO escritor (nombre, apellidos,email,edad,nacionalidad) values (?,?,?,?,?)');
+            $query->bindParam(1, $this->nombre, PDO::PARAM_STR);
+			$query->bindParam(2, $this->apellidos, PDO::PARAM_STR);
+            $query->bindParam(3, $this->email, PDO::PARAM_STR);
+            $query->bindParam(4, $this->edad, PDO::PARAM_INT);
+            $query->bindParam(4, $this->nacionalidad, PDO::PARAM_STR);
+			$query->execute();
+			$this->con->close();
+		}
+        catch(PDOException $e) {
+	        echo  $e->getMessage();
+	    }
+	}
+
+    public function update(){
+		try{
+			$query = $this->con->prepare('UPDATE escritor SET nombre = ?, apellidos = ?, email = ?, edad = ?, nacionalidad = ? WHERE id = ?');
+			$query->bindParam(1, $this->nombre, PDO::PARAM_STR);
+			$query->bindParam(2, $this->apellidos, PDO::PARAM_STR);
+            $query->bindParam(3, $this->email, PDO::PARAM_STR);
+            $query->bindParam(4, $this->edad, PDO::PARAM_INT);
+            $query->bindParam(5, $this->nacionalidad, PDO::PARAM_STR);
+            $query->bindParam(6, $this->id, PDO::PARAM_INT);
+			$query->execute();
+			$this->con->close();
+		}
+        catch(PDOException $e){
+	        echo  $e->getMessage();
+	    }
+	}
+
+	//obtenemos usuarios de una tabla con postgreSql
+	public function get(){
+		try{
+            if(is_int($this->id)){
+                $query = $this->con->prepare('SELECT * FROM escritor WHERE id = ?');
+                $query->bindParam(1, $this->id, PDO::PARAM_INT);
+                $query->execute();
+    			$this->con->close();
+    			return $query->fetch(PDO::FETCH_OBJ);
+            }
+            else{
+                $query = $this->con->prepare('SELECT * FROM escritor');
+    			$query->execute();
+    			$this->con->close();
+    			return $query->fetchAll(PDO::FETCH_OBJ);
+            }
+		}
+        catch(PDOException $e){
+	        echo  $e->getMessage();
+	    }
+	}
+
+    public function delete(){
+        try{
+            $query = $this->con->prepare('DELETE FROM escritor WHERE id = ?');
+            $query->bindParam(1, $this->id, PDO::PARAM_INT);
+            $query->execute();
+            $this->con->close();
+            return true;
+        }
+        catch(PDOException $e){
+            echo  $e->getMessage();
+        }
+    }
+
+    public static function baseurl() {
+         return stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://' . $_SERVER['HTTP_HOST'] . "/Mexcritores/";
+    }
+
+    public function checkUser($user) {
+        if( ! $user ) {
+            header("Location:" . Writer::baseurl() . "app/list.php");
+        }
+    }
+}
+?>
